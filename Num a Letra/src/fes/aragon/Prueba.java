d270f5ca31bc69bd0b9639069d85b8ed9fb4ac71
pedroanca <pedroantunezca@gmail.com>
2018-05-04 15:34:25 -0500

    Numero a Letra

---
+++ Num a Letra/src/fes/aragon/Prueba.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fes.aragon;
+
+/**
+ *
+ * @author Pedro Antunez
+ */
+import java.io.*;
+
+public class Prueba{
+	public static void main(String Arg[ ]) throws IOException{
+	NumALetra numero;
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	int num;
+	String res;
+        System.out.print("Ingrese numero : ");
+        num = Integer.parseInt(in.readLine( ));
+		numero = new NumALetra(num);
+		res = numero.convertirLetras(num);
+		System.out.print(res);
+		System.out.println("\n");
+	}	
+}
+
